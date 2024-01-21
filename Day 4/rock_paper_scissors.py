e3d82960509fa886eb8ecc9398ee0b517b71b8ea
LamaWaffle <matty.eberhard@gmail.com>
2024-01-21 11:21:11 +1100

    final change

--- Day 4/rock_paper_scissors.py
+++ Day 4/rock_paper_scissors.py
@@ -28,35 +28,49 @@ scissors = '''
 
 import random
 
+user_choice = int((input("What do you choose?\n0 for Rock, 1 for Paper, 2 for Scissors.\n")))
+possible_actions = [rock, paper, scissors]                      # Define the actions in a list.
+users_actions = (possible_actions[user_choice])                 # From the list (possible actions), pick out the indexed number that the user chose.
 
-user_actions = int((input("What do you choose?\n0 for Rock\n1 for Paper\n2 for Scissors.\n")))
-possible_actions = str([rock, paper, scissors])              # Define the actions in a list.
-user_input = (possible_actions[user_actions])           # From the list (possible actions), pick out the indexed numbed.
-
-computer_actions = (random.choice(possible_actions))    # CPU random choice is actioned.
-
+computer_actions = (random.choice(possible_actions))            # CPU random choice is actioned.
 
 
-if user_actions == computer_actions:
-    print(f"Both players selected {user_actions}, its a tie!")
+if users_actions == computer_actions:
+    print(users_actions)
+    print(f"Computer chose: {computer_actions}")
+    print("Its a tie!")
 
-elif user_actions == "rock":
-    if computer_actions == "scissors":
+elif users_actions == rock:
+    if computer_actions == scissors:
+        print(users_actions)
+        print(f"Computer chose: {computer_actions}")
         print("You win!")
     else:
-        print("You lose!")
+        print(users_actions)
+        print(f"Computer chose: {computer_actions}")
+        print(computer_actions)
 
 
-elif user_actions == "paper":
-    if computer_actions == "rock":
+elif users_actions == paper:
+    if computer_actions == rock:
+        print(users_actions)
+        print(f"Computer chose: {computer_actions}")
         print("You win!")
     else:
-        print("You lose!")
+        print(users_actions)
+        print(f"Computer chose: {computer_actions}")
+        print("You Lose!")
 
 
-
-elif user_actions == "scissors":
-    if computer_actions == "paper":
+elif users_actions == scissors:
+    if computer_actions == paper:
+        print(users_actions)
+        print(f"Computer chose: {computer_actions}")
         print("You win!")
     else:
-        print("You lose!")
+        print(users_actions)
+        print(f"Computer chose: {computer_actions}")
+        print("You Lose!")
+
+
+
