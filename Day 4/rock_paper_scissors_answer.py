019df4c6b2c8489b617cbdd7c8d285b8dcc66343
LamaWaffle <matty.eberhard@gmail.com>
2024-01-21 12:33:01 +1100

    Reformatted the code with PyCharm. Looks alot nicer now.

--- Day 4/rock_paper_scissors_answer.py
+++ Day 4/rock_paper_scissors_answer.py
@@ -27,34 +27,33 @@ scissors = '''
 ---.__(___)
 '''
 
-game_images = [rock, paper, scissors]                        # This is putting the images into a list.
+game_images = [rock, paper, scissors]  # This is putting the images into a list.
 
 # User logic
-user_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))       # Collect users input, convert to an integer, prints action.
+user_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))  # Collect
+# users input, convert to an integer, prints action.
 if user_choice >= 3 or user_choice < 0:
-    print("You typed an invalid number - GAME OVER!")        # This is a catch in-case the users input was invalid. If the result is invalid, it ends the game.
+    print("You typed an invalid number - GAME OVER!")  # This is a catch in-case the users input was invalid.
+    # If the result is invalid, it ends the game.
 
 else:
-    print(game_images[user_choice])                           
-
+    print(game_images[user_choice])
 
     # Computer logic
-    computer_choice = random.randint(0, 2)                       # Generate a random number from 0, 1 and 2, assign it to computers_choice.
+    computer_choice = random.randint(0, 2)  # Generate a random number from 0, 1 and 2, assign
+    # it to computers_choice.
     print("Computer chose:")
     print(game_images[computer_choice])
 
-
-    if user_choice == 0 and computer_choice == 2:              # This is an override since generally 2 is greater than 0, were making an exception here.
+    if user_choice == 0 and computer_choice == 2:  # This is an override - were making an exception here.
         print("You win!")
-    elif computer_choice == 0 and user_choice == 2:              
+    elif computer_choice == 0 and user_choice == 2:
         print("You lose!")
 
-
-    elif user_choice > computer_choice:                          # These are the general rules, 2 beats 0.
+    elif user_choice > computer_choice:  # These are the general rules, 2 beats 0.
         print("You win!")
     elif computer_choice > user_choice:
         print("You lose.")
 
-
-    elif computer_choice == user_choice:                         # A catch for the draw outcome.
+    elif computer_choice == user_choice:  # A catch for the draw outcome.
         print("Its a draw!")
