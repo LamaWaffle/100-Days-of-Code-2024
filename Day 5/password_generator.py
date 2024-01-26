9b4fa1e65e7bdea9606f4bc4888b9444e19b96ac
LamaWaffle <matty.eberhard@gmail.com>
2024-01-26 18:17:00 +1100

    day 5 complete

--- Day 5/password_generator.py
+++ Day 5/password_generator.py
@@ -9,8 +9,8 @@ symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
 print("Welcome to the PyPassword Generator!")
 nr_letters = int(input("How many letters would you like in your password?\n"))
-nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
+nr_symbols = int(input(f"How many symbols would you like?\n"))
 
 password_list = []
 
