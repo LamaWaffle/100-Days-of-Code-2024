acaa5e60385a645d2d62e1032c0ddf0dccca4301
LamaWaffle <matty.eberhard@gmail.com>
2024-02-03 09:56:07 +1100

    update

---
+++ Notes/keyboard_list.py
@@ -0,0 +1,12 @@
+
+wooting_barebones_kit = [["Wooting 60HE+ Module", 216], ["Lekker Switch - Linear60", 28],]
+
+wooting_full_keyboard = ["Wooting 60HE+ keyboard", 265]
+
+wooting_extras = ["Wooting Switch Lube + Tools", 46.80]
+
+keyboard_case = [["Tofu 60 Redux", 59], ["Case Shipping", 28]]
+
+shipping = 46
+
+
