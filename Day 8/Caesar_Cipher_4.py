9461ccf22d3032651424c3829c428cc7e75716db
LamaWaffle <103343056+LamaWaffle@users.noreply.github.com>
2024-02-20 11:10:31 +1100

    Remove unused imports and logo

--- Day 8/Caesar_Cipher_4.py
+++ Day 8/Caesar_Cipher_4.py
@@ -1,4 +1,3 @@
-from art import logo
 
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 
             'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 
@@ -20,12 +19,9 @@ def caesar(start_text, shift_amount, cipher_direction):
     print(f"Here's the {cipher_direction}d result: {end_text}")
 
 
-print(logo)
-
-
 should_end = False
 while not should_end:
-    
+
     direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
     text = input("Type your message:\n").lower()
     shift = int(input("Type the shift number:\n"))
