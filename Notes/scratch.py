1ead7ea940a94257e22b73a5fae1b827abbf6ea4
LamaWaffle <103343056+LamaWaffle@users.noreply.github.com>
2024-02-20 11:06:25 +1100

    Add robot switch functionality

--- Notes/scratch.py
+++ Notes/scratch.py
@@ -1,1 +1,8 @@
 
+switched_on = True
+while switched_on:
+    print("I am a robot, i am switched on.")
+    answer = input("Would you like to turn me off?\n")
+    if answer == "yes":
+        switched_on = False
+        
